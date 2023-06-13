451c86b30dda3b8e7890305af0a6054fb49fdc1d
zchhhhh <2069820192@qq.com>
2023-06-13 22:05:36 +0800

    feat: 新增角色管理

---
+++ src/api/system/sysRole.js
@@ -0,0 +1,59 @@
+/*
+角色管理相关的API请求函数
+*/
+import request from '@/utils/request'
+
+const api_name = '/admin/system/sysRole'
+
+export default {
+  /*
+  获取角色分页列表(带搜索)
+  */
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get',
+      // 如果是普通的对象参数写法， params：对象参数名称
+      // 如果是json格式传递，data：对象参数名称
+      params: searchObj
+    })
+  },
+  // 角色删除
+  removeById(id) {
+    return request({
+      url: `${api_name}/remove/${id}`,
+      method: 'delete'
+    })
+  },
+  // 添加角色
+  saveRole(role) {
+    return request({
+      url: `${api_name}/save`,
+      method: 'post',
+      data: role
+    })
+  },
+  // 根据id查询
+  getById(id) {
+    return request({
+      url: `${api_name}/get/${id}`,
+      method: 'get'
+    })
+  },
+  // 更改用户
+  updateById(role) {
+    return request({
+      url: `${api_name}/update`,
+      method: 'put',
+      data: role
+    })
+  },
+  // 批量删除
+  batchRemove(idList) {
+    return request({
+      url: `${api_name}/batchRemove`,
+      method: `delete`,
+      data: idList
+    })
+  }
+}
